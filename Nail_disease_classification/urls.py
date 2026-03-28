b5b9939c5d03090fcfab46f049f601f44483d081
ajjadapavan143-png <ajjadapavan143@gmail.com>
2026-03-28 16:52:11 +0530

    fix prediction page error

--- Nail_disease_classification/urls.py
+++ Nail_disease_classification/urls.py
@@ -7,7 +7,6 @@ from . import views as mainView
 from admins import views as admins
 from users import views as usr
 
-# Force reload triggered
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -20,13 +19,12 @@ urlpatterns = [
 
     # Admin views
     path("AdminHome/", admins.AdminHome, name="AdminHome"),
-    # path("AdminLoginCheck/", admins.AdminLoginCheck, name="AdminLoginCheck"),
-    path('RegisterUsersView/', admins.RegisterUsersView, name='RegisterUsersView'),
-    path('ActivaUsers/', admins.ActivaUsers, name='ActivaUsers'),
-    path('DeleteUser/', admins.DeleteUser, name='DeleteUser'),
-    path('AdminPredictionHistory/', admins.AdminPredictionHistory, name='AdminPredictionHistory'),
+    path("RegisterUsersView/", admins.RegisterUsersView, name="RegisterUsersView"),
+    path("ActivaUsers/", admins.ActivaUsers, name="ActivaUsers"),
+    path("DeleteUser/", admins.DeleteUser, name="DeleteUser"),
+    path("AdminPredictionHistory/", admins.AdminPredictionHistory, name="AdminPredictionHistory"),
 
-    # User Views
+    # User views
     path("UserRegisterActions/", usr.UserRegisterActions, name="UserRegisterActions"),
     path("UserLoginCheck/", usr.UserLoginCheck, name="UserLoginCheck"),
     path("UserHome/", usr.UserHome, name="UserHome"),
@@ -35,6 +33,6 @@ urlpatterns = [
     path("UserPredictionHistory/", usr.UserPredictionHistory, name="UserPredictionHistory"),
 ]
 
-# Media serve in development
+# Media serve in development only
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
